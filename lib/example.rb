d76012f04560e2c1ff8b59b95157b4f64debe971
Kevin Mathias <komathias1@gmail.com>
2020-03-23 18:20:23 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -16,15 +16,15 @@ puts "Hello World"
 phrase = "Hello World"
 def say_hello_ten_times
   phrase = "Hello World"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
 end
 say_hello_ten_times
