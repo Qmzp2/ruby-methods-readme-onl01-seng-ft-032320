a140adc428253496ba67f5b3b08809f433631ed0
Kevin Mathias <komathias1@gmail.com>
2020-03-23 18:15:36 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -11,3 +11,20 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
+
+puts "Hello World"
+phrase = "Hello World"
+def say_hello_ten_times
+  phrase = "Hello World"
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+end
+say_hello_ten_times
